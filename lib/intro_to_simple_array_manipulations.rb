aaad99d1f7f2ccb8a73c0b676ad9ad17bcc13350
Allen Huang <alhuang1288@gmail.com>
2019-08-23 15:18:58 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,9 @@
+using_push = ["allen", "zolboo"]
+new = "lilo"
+using_push.push("lilo")
+
+using_unshift = ["allen", "zolboo", "lilo"]
+using_unshift.unshift("coco")
+
+using_pop = ["allen", "zolboo", "lilo"]
+using_pop.pop
